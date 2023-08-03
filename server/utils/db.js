010cf05dd5b3254f4316a4a60e30dd52b08e1f2f
Olami007 <oladimejibells@gmail.com>
2023-08-03 17:52:53 +0100

    server side upload updated

---
+++ server/utils/db.js
@@ -0,0 +1,10 @@
+// import mongoose from "mongoose";
+
+// export default async function connect() {
+//   try {
+//     await mongoose.connect(process.env.MONGO);
+//     console.log("database connected");
+//   } catch (error) {
+//     throw new Error("connection failed");
+//   }
+// }
