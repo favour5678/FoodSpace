de54c43197f5f7b1fe47eabcbee2bed01b550e68
Oshoaro Favour <oshoarofavour@gmail.com>
2023-09-16 21:14:03 +0100

    made a change

--- src/components/BottomNavbar.jsx
+++ src/components/BottomNavbar.jsx
@@ -9,7 +9,7 @@ const BottomNavbar = () => {
   return (
     <div className='container max-w-full fixed bottom-0 md:hidden'>
         <Link className='text-2xl text-white w-8 h-8 flex items-center justify-center border-white border-2 rounded-[100%] absolute -top-4 left-2 bg-red-700'>
-            <Link><AiFillHome /></Link>
+            <button className='cursor-pointer'><AiFillHome /></button>
         </Link>
         <div className='bg-red-700 py-2 flex items-center justify-evenly text-2xl text-white'>
             <Link><GiKnifeFork /></Link>
